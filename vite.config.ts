25d1c7cc0e3dd71eec24e645c1733088f965a01b
499Circlepzzle <499johan@gmail.com>
2026-01-13 05:53:27 +0200

    Set base path for Vite configuration
    Signed-off-by: 499Circlepzzle <499johan@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }) => ({ 
+  base:"/fractionchart/",
   server: {
     host: "::",
     port: 5000,
